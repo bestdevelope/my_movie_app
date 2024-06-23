f6da35e53105264c840b3050471815861fef5430
bestdevelope <leabouillot7@gmail.com>
2024-06-23 18:33:23 +0200

    afficher list de livre

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Movie from "./Movie";
+
+const LivreFav =[
+  {
+    id: "1 ",
+    name: "Demien",
+    img: "https://www.livredepoche.com/sites/default/files/images/livres/couv/9782253022916-001-T.jpeg"
+  },
+  { id: "2 ",
+    name: "La vie est belle",
+    img: " https://actualitte.com/uploads/images/9782070406234-475x500-1-61eaf7575e8ec789153864.jpg"
+  },
+  { id: "3 ",
+    name: "Ciel blue",
+    img: "https://www.livredepoche.com/sites/default/files/images/livres/couv/9782253934103-001-T.jpeg "
+  },
+  { id: "4 ",
+    name: "Totoro",
+    img: "https://m.media-amazon.com/images/I/81C62N5woGL._SY522_.jpg "
+  },
+  { id: "5",
+    name: "Chateu dans le ciel",
+    img: "https://cdn.cultura.com/media/pim/TITELIVE/10_9782344033739_1_75.jpg"
+  },
+]
+
+function Livre({name, images}) {
+  return (
+    <div>
+      <h2>Titre de livre: {name}</h2>
+      <img src={images} alt={name} />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Livre favorite</h1>
+      {LivreFav.map(favorite => (<Livre key={favorite.id} name = {favorite.name} images = {favorite.img}/>))}
     </div>
   );
 }
